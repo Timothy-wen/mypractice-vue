85beea0c1a008acfdc70482c5c1d8cc32370095d
Timothy-wen <2287141954@qq.com>
2022-12-20 09:46:19 +0800

    home页面横幅图片

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import './assets/css/global.css'
 import './assets/css/iconfont.css'
 import './assets/css/index.css'
 import './assets/css/me.css'
-import './plugins/element.js'
+//import './plugins/element.js'
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
